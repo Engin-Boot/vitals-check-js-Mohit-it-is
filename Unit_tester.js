a62775af94800e98152ac4a0ad45dd9dfd02a26e
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-08-28 11:34:25 +0530

    added testcases

--- Unit_tester.js
+++ Unit_tester.js
@@ -4,6 +4,10 @@ const vitalsAreOk = require('./checker');
 (function(){
     expect(vitalsAreOk(100, 95, 70)).to.be.true;
     expect(vitalsAreOk(50, 95, 70)).to.be.false;
+    expect(vitalsAreOk(160, 95, 70)).to.be.false;
+    expect(vitalsAreOk(100, 50, 70)).to.be.false;
+    expect(vitalsAreOk(100, 95, 25)).to.be.false;
+    expect(vitalsAreOk(100, 95, 100)).to.be.false;
     
     console.log('checker is done');
 
