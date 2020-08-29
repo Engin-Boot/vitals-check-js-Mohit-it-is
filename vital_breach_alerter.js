60c584a10db318aee482384f921287648d45184b
Mohit-it-is <mohitkr.mnnit@gmail.com>
2020-08-29 11:47:55 +0530

    Complete Assignment

--- vital_breach_alerter.js
+++ vital_breach_alerter.js
@@ -1,8 +1,8 @@
-const staff_json = require('./staff.json');
+const staff_json = require('./staff_alert.json');
 
 function alert(alertvitaltype){
     let vitaltype = Object.keys(alertvitaltype)[0];
-    
+
     //console.log("Call" + " " + staff_json[vitaltype] + " bpm" + " " +alertvitaltype[vitaltype]);
 }
 
